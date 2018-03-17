9bc7a688575e30ff21f095b92c421d6ba3263e6e
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-17 14:19:25 -0500

    PRACTICE

---
+++ 3.17.18.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int fill[6];
+
+	for (int i=0; i<5; i++){
+		cout << "Enter number " << i+1 << endl;
+		cin >> fill[i];
+	}
+cout << fill[0,1,2,3,4,5];
+	
+	return 0;
+}
