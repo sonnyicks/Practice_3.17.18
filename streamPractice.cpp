132cad25860f1b342a39ab3cdb17291896ea80f1
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-18 10:03:06 -0500

    PRACTICE

--- streamPractice.cpp
+++ streamPractice.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
 ofstream numlist;
 numlist.open("NumbersList.dat");
+if (numlist.fail()){
+	cout << "Well, that didn't work...";
+	exit (1);
+}
 int highnum = 0;
 int count = 0;
 int total = 0;
@@ -22,6 +26,7 @@ for (int i=1; i<=highnum; i++){
 	total+=i;
 	numlist << total << " ";
 }
+numlist.close();
 	
 	return 0;
 }
